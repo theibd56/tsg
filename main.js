7b01981c0e4ff9a68aaba0d406a5173706a9afce
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-12-09 21:25:50 +0700

    products section has been created

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+import Swiper from 'swiper/bundle';
+
 import './sass/_app.scss';
+import 'swiper/css/bundle';
+
+const productsSlider = new Swiper('.products-slider', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 600,
+    loop: false,
+    breakpoints: {
+        // 300: {
+        //     slidesPerView: 2.25,
+        // },
+        // 576: {
+        //     slidesPerView: 3,
+        // },
+        // 992: {
+        //     slidesPerView: 4,
+        // },
+    },
+})

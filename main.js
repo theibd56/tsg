8b471b8f2ddddf0d527b5c48b26a42c20cb9f408
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-12-10 22:37:23 +0700

    review section has been created

--- main.js
+++ main.js
@@ -20,3 +20,30 @@ const productsSlider = new Swiper('.products-slider', {
         // },
     },
 })
+
+const reviewSlider = new Swiper('.review-slider', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 600,
+    loop: false,
+    navigation: {
+        prevEl: '.review-slider__prev',
+        nextEl: '.review-slider__next',
+    },
+    pagination: {
+        el: '.review-slider__pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        // 300: {
+        //     slidesPerView: 2.25,
+        // },
+        // 576: {
+        //     slidesPerView: 3,
+        // },
+        // 992: {
+        //     slidesPerView: 4,
+        // },
+    },
+})
+
